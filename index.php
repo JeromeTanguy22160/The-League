47d84a012c6ef4b8be2764584f9bddc6c8745799
AnthelmeJa <anthelme.jarreau35@gmail.com>
2025-07-03 15:03:58 +0000

    test

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once __DIR__ . '/config/vendor/autoload.php';
-
-$loader = new \Twig\Loader\FilesystemLoader('templates');
-        $twig = new \Twig\Environment($loader,[
-            'debug' => true,
-]);
-
+    require_once __DIR__ . '/vendor/autoload.php';
+    
+    require_once __DIR__. '/config/autoload.php';
+    
+    $routeur = new Routeur();
+    
+    $router->handleRequest($_GET);
 
 ?>
