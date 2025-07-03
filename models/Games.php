7038117bf3875d53e92a91d1a056a1bf74684ce4
Tom9603 <tom.ochietti@gmail.com>
2025-07-03 09:14:47 +0000

    Games et Media

---
+++ models/Games.php
@@ -0,0 +1,68 @@
+<?php
+
+class Games
+{
+    private ?int $id = null;
+    private string $name;
+    private \DateTimeImmutable $created_at;
+    private int $team_1;
+    private int $team_2;
+    private int $winner;
+
+    public function __construct(string $name, \DateTimeImmutable $created_at, int $team_1, int $team_2, int $winner) {
+        $this->name = $name;
+        $this->created_at = $created_at;
+        $this->team_1 = $team_1;
+        $this->team_2 = $team_2;
+        $this->winner = $winner;
+    }
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function setName(string $name): void {
+        $this->name = $name;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): void {
+        $this->created_at = $created_at;
+    }
+
+    public function getTeam1(): int {
+        return $this->team_1;
+    }
+
+    public function setTeam1(int $team_1): void {
+        $this->team_1 = $team_1;
+    }
+
+    public function getTeam2(): int {
+        return $this->team_2;
+    }
+
+    public function setTeam2(int $team_2): void {
+        $this->team_2 = $team_2;
+    }
+
+    public function getWinner(): int {
+        return $this->winner;
+    }
+
+    public function setWinner(int $winner): void {
+        $this->winner = $winner;
+    }
+}
+?>
