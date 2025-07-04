ce216fd6184d1d53fad05cc60067cdec9fa90af4
AnthelmeJa <anthelme.jarreau35@gmail.com>
2025-07-04 09:43:36 +0000

    error pull

--- controlers/TeamController.php
+++ controlers/TeamController.php
@@ -18,7 +18,9 @@
         
         $teams = $teamManagerTeams -> findAll();
         
-        require "templates/teams.html.twig";
+        echo $twig->render('teams.html.twig', [
+            'teams' => $teams
+        ]);
 
         }
         
